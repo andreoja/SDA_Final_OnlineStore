3f55df5edd059b4ae9c85d75e3937b6b9de80d01
Andre <andredja@hot.ee>
2020-08-08 09:12:49 +0300

    added two categories to models

--- webshop/pages/models.py
+++ webshop/pages/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Category(models.Model):
+class CategoryPart(models.Model):
     class Meta:
         verbose_name_plural = "categories"
 
@@ -11,13 +11,26 @@ class Category(models.Model):
     def __str__(self):
         return self.name + ' - prerishable:' + str(self.perishable)
 
+class CategoryCarBrand(models.Model):
+    class Meta:
+        verbose_name_plural = "categories"
+
+    name = models.CharField(max_length=15)
+    perishable = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name + ' - prerishable:' + str(self.perishable)
+
 
 class Product(models.Model):
     part_number = models.CharField(max_length=15)
-    car_brand = models.CharField(max_length=30)
+    category_car_brand = models.ForeignKey(
+        CategoryCarBrand, on_delete=models.DO_NOTHING, null=True, blank=True)
     ean_code = models.CharField(max_length=20)
     product_name_EN = models.CharField(max_length=40)
     product_name_EE = models.CharField(max_length=40)
+    category_part = models.ForeignKey(
+        CategoryPart, on_delete=models.DO_NOTHING, null=True, blank=True)
     product_info = models.TextField(null=True, blank=True)
     quantity = models.PositiveIntegerField(default=0)
     warehouse_address = models.CharField(max_length=10)
